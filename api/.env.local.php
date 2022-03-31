665bd8446c5b5e1e637ce6a43ef202798f81f06b
lucasmartinelle <lucaspro.martinelle@gmail.com>
2022-03-31 19:51:57 +0200

    regenerate APP_SECRET automaticly

--- api/.env.local.php
+++ api/.env.local.php
@@ -4,7 +4,7 @@
 
 return array (
   'APP_ENV' => 'prod',
-  'NzU5ZDUwYmNlMjA3Nzg2ZWU3NDE5YzA2' => '94a41eb1235ad8f061651a6c7ded1ef4',
+  'APP_SECRET' => 'appsecret',
   'DATABASE_URL' => 'mysql://apiuser:apiuser@mariadb:3306/api?serverVersion=mariadb-10.4.19',
   'JWT_SECRET_KEY' => 'secretkey',
   'CORS_ALLOW_ORIGIN' => '^https?://(localhost|127\\.0\\.0\\.1)(:[0-9]+)?$',
--- api/installation.sh
+++ api/installation.sh
@@ -3,6 +3,7 @@
 cd /var/www/api
 
 sed -i 's/secretkey/'$(echo $RANDOM | sha256sum | base64 | head -c 32 ; echo)'/' .env.local.php
+sed -i 's/appsecret/'$(echo $RANDOM | sha256sum | base64 | head -c 32 ; echo)'/' .env.local.php
 
 composer install --no-interaction
 
